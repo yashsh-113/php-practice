448c32e861059513a8974b65d6ce0eafa96f83f5
yashs-113 <yashs20002@gmail.com>
2021-10-25 15:25:07 +0530

    changes

--- index.php
+++ index.php
@@ -11,12 +11,9 @@
 </head>
 
 <body>
-
-    <div>
-        <?php
-        include 'navbar.php';
-        ?>
-    </div>
+    <?php
+    include 'components/navbar.php';
+    ?>
 
 
     <div>
@@ -30,7 +27,7 @@
             <div class="col-3">
                 <div class="d-none d-md-block">
                     <?php
-                        include 'sidebar.php';
+                        include 'components/sidebar.php';
                     ?>
                 </div>
             </div>
@@ -43,34 +40,23 @@
                 </p> 
                 
                 <div>
-        <?php
-            include 'card.php';
-        ?>
-    </div>
-
+                <?php
+                    include 'components/card.php';
+                ?>
             </div>
+
+        </div>
     </div>
 
-
-   
-
-    <div>
         <?php
-            include 'footer.php';
+            include 'components/footer.php';
         ?>
-    </div> 
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
-    </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
-    </script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
-    </script>
+    <script src="bootstrap.min.js"
+        
+    
     
 </body>
 
